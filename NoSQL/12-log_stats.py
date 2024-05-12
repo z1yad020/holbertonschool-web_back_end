368d251b988fd890a35e502ce1418154bc1d90f7
Ziyad Ibrahim <ziyad.ibrahim.112358@gmail.com>
2024-05-12 12:08:14 +0400

    name main

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,18 +5,18 @@ log doc
 """
 from pymongo import MongoClient
 
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_ng = client.logs.nginx
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-logs_ng = client.logs.nginx
+    count = logs_ng.estimated_document_count()
 
-count = logs_ng.estimated_document_count()
+    print(f'{count} logs')
+    print('Methods:')
 
-print(f'{count} logs')
-print('Methods:')
+    for mt in ("GET", "POST", "PUT", "PATCH", "DELETE"):
+        print('\tMethod {}: {}'.format(mt,
+              logs_ng.count_documents({'method': mt})))
 
-for mt in ("GET", "POST", "PUT", "PATCH", "DELETE"):
-    print('\tMethod {}: {}'.format(mt,
-          logs_ng.count_documents({'method': mt})))
-
-print('{} status check'.format(
-      logs_ng.count_documents({'method': 'GET', 'path': '/status'})))
+    print('{} status check'.format(
+          logs_ng.count_documents({'method': 'GET', 'path': '/status'})))
