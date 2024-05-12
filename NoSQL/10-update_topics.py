c0fb6b6cb0dc465f2bdc205de08f9e669c7c5c25
Ziyad Ibrahim <ziyad.ibrahim.112358@gmail.com>
2024-05-12 11:31:04 +0400

    $set

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -7,4 +7,4 @@ update doc
 
 def update_topics(mongo_collection, name, topics):
     """update doc"""
-    mongo_collection.update_many({'name': name}, {'topics': topics})
+    mongo_collection.update_many({'name': name}, { $set: {'topics': topics}})
