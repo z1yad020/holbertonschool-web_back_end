f892a6e8f798431ce4e73bd1cc4cc967d320fa49
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-05-12 21:03:34 +0000

    task 10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arr = [...array];
   for (const idx of arr) {
-    const value = arr[idx];
-    arr[idx] = appendString + value;
+    const value = arr.indexOf(idx);
+    arr[value] = appendString + idx;
   }
 
   return arr;
--- ES6_basic/main.js
+++ ES6_basic/main.js
@@ -1,3 +1,3 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import appendToEachArrayValue from './10-loops.js';
 
-console.log(getBudgetObject(400, 700, 900));
+console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
