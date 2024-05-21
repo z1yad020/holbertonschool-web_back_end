345960d206a33644307a6e845883764b96d8a883
ziyad <ziyad.ibrahim.112358@gmail.com>
2024-05-21 09:05:28 +0000

    indent

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,9 +4,9 @@ import signUpUser from './4-user-promise';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const proms = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   return Promise.allSettled(proms).then((results) => (
-      results.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : String(result.reason),
-      }))
-    ));
+    results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : String(result.reason),
+    }))
+  ));
 }
