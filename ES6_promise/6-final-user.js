8ec6c6d59699d0829b82118dc3de4fa3ccbc96df
ziyad <ziyad.ibrahim.112358@gmail.com>
2024-05-21 08:54:05 +0000

    i am dumb(

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,6 +2,6 @@ import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const proms = [uploadPhoto(firstName, lastName), signUpUser(fileName)];
+  const proms = [uploadPhoto(fileName), signUpUser(firstName, lastName)];
   return Promise.allSettled(proms);
 }
