4da9abe61cf3193de69c318bbf3cd690938edb98
ziyad <ziyad.ibrahim.112358@gmail.com>
2024-05-22 15:42:44 +0000

    check type

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (typeof startString !== 'string' || startString === '') return '';
 
   const list = [];
 
