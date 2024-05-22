caaa7723b6f92a877f0d0183e6538a4932e9f620
ziyad <ziyad.ibrahim.112358@gmail.com>
2024-05-22 13:24:07 +0000

    manual throw

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -2,10 +2,9 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8View = new Int8Array(buffer);
 
-  try {
-    int8View[position] = value;
-  } catch (e) {
-    throw new Error('Position outside range');
-  }
+  if (position >= length) throw new Error('Position outside range');
+
+  int8View[position] = value;
+
   return new DataView(buffer);
 }
